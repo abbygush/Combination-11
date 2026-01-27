3548a6a07f07a7a00c2efe100ad24f1dd0c57b0f
Nebiyu Samuel <nebiyusamuel809@gmail.com>
2026-01-27 15:49:31 +0300

    Auction code with stack

---
+++ Problem 53/Auction.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+record Bid(String user, int amount) {
+    public String toString() { return amount + ""; }
+}
+
+public class Auction {
+    public static void main(String[] args) {
+        var stack = new ArrayDeque<Bid>();
+        var scanner = new Scanner(System.in);
+        
+        while (true) {
+            System.out.print("> ");
+            String[] p = scanner.nextLine().split(" ");
+            
+            switch (p[0]) {
+                case "BID" -> {
+                    int amt = Integer.parseInt(p[2]);
+                    if (!stack.isEmpty() && amt <= stack.peek().amount) {
+                        System.out.println("Error: Too low.");
+                    } else {
+                        stack.push(new Bid(p[1], amt));
+                        System.out.println("Current: " + stack.peek().user + " (" + stack.peek().amount + ")");
+                        System.out.println("Stack: " + stack);
+                    }
+                }
+                case "WITHDRAW" -> {
+                    var withdrawn = stack.pop();
+                    System.out.println(withdrawn.user + " retracted.");
+                    if (stack.isEmpty()) System.out.println("No bids remaining.");
+                    else System.out.println("Reverted to " + stack.peek().user + " (" + stack.peek().amount + ")");
+                    System.out.println("Stack: " + stack);
+                }
+                case "CURRENT" -> {
+                    if (stack.isEmpty()) System.out.println("No bids yet.");
+                    else System.out.println("Current: " + stack.peek().user + " (" + stack.peek().amount + ")");
+                }
+                case "EXIT" -> { return; }
+            }
+        }
+    }
+}
